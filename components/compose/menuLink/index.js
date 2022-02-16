f319e6c920012f0ddcbfe5324838d2b32ad9c927
Naslimin <naslimin@gmail.com>
2022-02-16 17:57:42 +0700

    menuLink

---
+++ components/compose/menuLink/index.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import styles from './index.module.css'
+import Link from 'next/link'
+import { useRouter } from "next/router";
+import { useState } from 'react'
+export default function MenuLink({ to, icon, iconHover, children }) {
+  const [isHovering, setIsHovered] = useState(false);
+  const onMouseEnter = () => setIsHovered(true);
+  const onMouseLeave = () => setIsHovered(false);
+  const router = useRouter();
+  return (
+    <Link href={to}>
+      <div onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave} className={router.pathname == `/${to}` ? `${styles.menuLink} ${styles.active}` : styles.menuLink}>
+        {
+          isHovering || router.pathname == `/${to}`?
+            <Image src={iconHover} alt="icon menu" width="27" height="30" />
+            :
+            <Image src={icon} alt="icon menu" width="27" height="30" />
+        }
+
+        <div className={styles.menuLabel}>{children}</div>
+      </div>
+    </Link>
+  )
+}
