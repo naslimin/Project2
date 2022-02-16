2a54fb25b6245093977971703c8d82abf7b727fd
Naslimin <naslimin@gmail.com>
2022-02-16 16:44:06 +0700

    loginPage

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Layout from '../components/layoutLogin'
+import UserBlank from '../components/compose/userBlank'
+import InputType from '../components/compose/inputType'
+import styles from './index.module.css'
+import RadioType from '../components/compose/radioType'
+export default function Index() {
+  return (
+    <>
+      <UserBlank className={styles.spacer}/>
+      <InputType className={styles.spacer} type="text" placeholder="Username (Email)"/>
+      <InputType className={styles.spacer} type="password" placeholder="Password"/>
+      <div className={styles.redioWarper}>
+        <RadioType name="loginType" checked={true} value="remember_me" onChange={e=>console.log(e)}>Remember me</RadioType>
+        <RadioType name="loginType" value='forgot_password' onChange={e=>console.log(e)}>Forgot password</RadioType>
+      </div>
+    </>
+  )
+}
+
+Index.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
