63aad841d2616219fd577f9845f5b3354e8fc108
Naslimin <naslimin@gmail.com>
2022-02-21 00:24:15 +0700

    menuLink update

--- components/compose/menuLink/index.js
+++ components/compose/menuLink/index.js
@@ -14,9 +14,9 @@ export default function MenuLink({ to, icon, iconHover, children }) {
         onMouseLeave={onMouseLeave} className={router.pathname == `/${to}` ? `${styles.menuLink} ${styles.active}` : styles.menuLink}>
         {
           isHovering || router.pathname == `/${to}`?
-            <Image src={iconHover} alt="icon menu" width="27" height="30" />
+            <Image src={iconHover} alt="icon menu" width="30" height="30" />
             :
-            <Image src={icon} alt="icon menu" width="27" height="30" />
+            <Image src={icon} alt="icon menu" width="30" height="30" />
         }
 
         <div className={styles.menuLabel}>{children}</div>
