813904ee6fb35390835205b81da7886c5ccf8932
Naslimin <naslimin@gmail.com>
2022-02-20 18:40:36 +0700

    BigCalendarCustom

---
+++ components/compose/BigCalendarCustom/events.js
@@ -0,0 +1,89 @@
+export default [
+    {
+        desc: 'All Day Event very long title',
+        'start': new Date(2022, 1, 1),
+        'end': new Date(2022, 1, 1),
+        type:'checkin'
+    },
+    {
+        desc: 'Long Event',
+        'start': new Date(2022, 1, 2),
+        'end': new Date(2022, 1, 2),
+        type:'checkrate'
+    },
+
+    {
+        desc: 'DTS STARTS',
+        'start': new Date(2022, 2, 4),
+        'end': new Date(2022, 2, 4),
+        type:'checkin'
+    },
+
+    {
+        desc: 'DTS ENDS',
+        'start': new Date(2022, 1, 6),
+        'end': new Date(2022, 1, 6),
+        type:'checkrate'
+    },
+
+    {
+        desc: 'Some Event',
+        'start': new Date(2022, 1, 9),
+        'end': new Date(2022, 1, 9),
+        type:'checkin'
+    },
+    {
+        desc: 'Big conference for important people',
+        'start': new Date(2022, 1, 11),
+        'end': new Date(2022, 1, 11),
+        type:'checkin'
+    },
+    {
+        desc: 'Meeting',
+        'start': new Date(2022, 1, 12),
+        'end': new Date(2022, 1, 12),
+        type:'checkrate'
+    },
+    {
+        desc: 'Lunch',
+        'start': new Date(2022, 1, 14),
+        'end': new Date(2022, 1, 14),
+        type:'leavework'
+    },
+    {
+        desc: 'Meeting',
+        'start': new Date(2022, 1, 16),
+        'end': new Date(2022, 1, 16),
+        type:'checkin'
+    },
+    {
+        desc: 'Happy Hour',
+        'start': new Date(2022, 1, 18),
+        'end': new Date(2022, 1, 18),
+        type:'checkin'
+    },
+    {
+        desc: 'Dinner',
+        'start': new Date(2022, 1, 19),
+        'end': new Date(2022, 1, 19),
+        type:'checkrate'
+    },
+    {
+        desc: 'Birthday Party',
+        'start': new Date(2022, 1, 21),
+        'end': new Date(2022, 1, 21),
+        type:'leavework'
+    },
+    {
+        desc: 'Birthday Party 2',
+        'start': new Date(2022, 1, 22),
+        'end': new Date(2022, 1, 22),
+        type:'checkrate'
+    },
+    {
+        desc: 'Birthday Party 3',
+        'start': new Date(2022, 1, 23),
+        'end': new Date(2022, 1, 23),
+        type:'checkin'
+    }
+]
---
+++ components/compose/BigCalendarCustom/index.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import events from './events';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import styles from './index.module.css'
+moment.locale('en-GB');
+const localizer = momentLocalizer(moment);
+const NameDay = { "Sun": "S", "Mon": "M", "Tue": "T", "Wed": "W", "Thu": "T", "Fri": "F", "Sat": "S" }
+let MyCustomHeader = ({ label }) => {
+  
+  return (
+    <div className={styles.fontDay}>{NameDay[label]}</div>
+  )
+}
+class MyCustomDateHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      www: null,
+      hhh: null
+    }
+    this.div = React.createRef();
+    this.item = events.findIndex(e => {
+      // if (this.props.date >= e.start && this.props.date <= e.end) {
+      //   return true
+      // }
+
+      if (this.props.date.getDate() >= e.start.getDate() && this.props.date.getDate() <= e.end.getDate()) {
+        return true
+      }
+    })
+    this.setM = false
+  }
+  componentDidMount() {
+    if (this.div.current.offsetHeight <= this.div.current.offsetWidth) {
+      this.setState({
+        www: this.div.current.offsetHeight
+      })
+
+      this.forceUpdate()
+    }
+    if (this.div.current.offsetHeight >= this.div.current.offsetWidth) {
+      this.setState({
+        hhh: this.div.current.offsetWidth
+      })
+
+      this.forceUpdate()
+    }
+  }
+
+  render() {
+    if (this.item != -1)
+      return (
+        <div ref={this.div} style={{ width: this.state.www, height: this.state.hhh }} className={`${styles.fontDate} ${events[this.item].type == "checkin" ? styles.checkin : events[this.item].type == "checkrate" ? styles.checkrate : ''} ${styles.focusDate}`}>{this.props.label}</div>
+      )
+    else return (
+      <div ref={this.div} style={{ width: this.state.www, height: this.state.hhh }} className={styles.fontDate}>{this.props.label}</div>
+    )
+  }
+}
+
+const ListEvents = () => {
+  return events.map(element => {
+    return <div className={`${styles.eventWarper}`}>
+      <div className={`${styles.eventWarperTitle}`}>
+        <p className={`${styles.eventTitle}`}>{element.desc}</p>
+        <p className={`${styles.eventMonth}`}>{moment(element.start).format("MMM")} {NameDay[moment(element.start).format('ddd')]}</p>
+      </div>
+      <div className={`${styles.eventDate}`}>{moment(element.start).format('dddd Do h:mm')}</div>
+    </div>
+  });
+}
+
+class CustomToolbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <div className={`${styles.warperCalendarHeader} ${styles.boxNav}`}>
+          <div onClick={() => this.props.onNavigate ? this.props.onNavigate('PREV') : undefined} className={`${styles.navbtn} ${styles.prev}`}>&#60;</div>
+          <div className={`${styles.CalendarHeaderText} text-center`}>{this.props.label}</div>
+          <div onClick={() => this.props.onNavigate ? this.props.onNavigate('NEXT') : undefined} className={`${styles.navbtn} ${styles.next}`}>&#62;</div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default function BigCalendarCustom() {
+  return (
+    <div className={styles.bgCalenDarOuter}>
+      <div className={styles.bgCalenDar}>
+        <div className={styles.warperCalendarHeader}>
+          <div className={`${styles.CalendarHeader1} ${styles.CalendarHeaderBox}`}>&#9776;</div>
+          <div className={`${styles.CalendarHeader2} ${styles.CalendarHeaderBox}`}>&#43;</div>
+        </div>
+
+        <Calendar
+          localizer={localizer}
+          views={{ month: true }}
+          events={[]}
+          step={60}
+          style={{ height: 500, width: '100%' }}
+          defaultDate={moment().toDate()}
+          components={{
+            month: { header: MyCustomHeader, dateHeader: MyCustomDateHeader },
+            toolbar: CustomToolbar
+          }}
+        />
+        <div className={`${styles.eventBox}`}>
+          <p className={`${styles.upcommingHeader}`}>Upcomming events this month</p>
+
+          <ListEvents />
+        </div>
+      </div>
+    </div>
+  )
+}
