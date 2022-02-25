ff21244956dc9733382ee0ccc601c749572988b2
Naslimin <naslimin@gmail.com>
2022-02-26 01:13:03 +0700

    fix ci user image

--- components/compose/AvatarUser/index.js
+++ components/compose/AvatarUser/index.js
@@ -5,13 +5,13 @@ export default function AvatarUser({ big }) {
   if (big) {
     return (
       <div className={styles.avatarBig}>
-        <img src="/demoImage/39cd1f2a-6b6b-4199-98ab-a80f629e5e51.jpeg" alt="Avatar" width="120" height="120" />
+        <img src="/icon/user.png" alt="Avatar" width="120" height="120" />
       </div>
     )
   } else {
     return (
       <div className={styles.avatar}>
-        <img src="/demoImage/39cd1f2a-6b6b-4199-98ab-a80f629e5e51.jpeg" alt="Avatar" width="60" height="60" />
+        <img src="/icon/user.png" alt="Avatar" width="60" height="60" />
       </div>
     )
   }
