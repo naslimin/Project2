be6b1d9544036e71b24ed40b9a8ec98d1e871a54
Naslimin <naslimin@gmail.com>
2022-02-27 02:10:41 +0700

    add func check login

--- components/layout.js
+++ components/layout.js
@@ -2,22 +2,48 @@ import Head from 'next/head'
 import ActivityBox from './compose/activityBox'
 import UserLoginBox from './compose/userLoginBox'
 import styles from './layout.module.css'
+import AlertBox from './compose/alertBox';
 import Sidebar from './sidebar'
+import fire from '../config/firebase';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 export default function Layout({ children, title }) {
-  return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <meta name="viewport" content="width=1920"></meta>
-      </Head>
-      <main className={styles.main}>
-        <Sidebar />
-        {children}
-        <div className={styles.secbar}>
-          <UserLoginBox />
-          <ActivityBox />
-        </div>
-      </main>
-    </>
-  )
+  const router = useRouter()
+  useEffect(() => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        children.props.setUserData(user)
+      } else {
+        router.push('/')
+      }
+    });
+  },[children.props.UserData])
+  if (children.props.UserData) {
+    return (
+      <>
+        <Head>
+          <title>{title}</title>
+          <meta name="viewport" content="width=1920"></meta>
+        </Head>
+        <main className={styles.main}>
+          <Sidebar props={children.props} />
+          {children}
+          <div className={styles.secbar}>
+            <UserLoginBox />
+            <ActivityBox />
+          </div>
+          {
+            children.props.showAlert ?
+              <AlertBox clickOut={(e) => children.props.setShowAlert(!e)}>
+                {children.props.AlertInner}
+              </AlertBox>
+              : null
+          }
+        </main>
+      </>
+    )
+  } else {
+    return (<>Not Login...</>)
+  }
 }
--- components/layoutLogin.js
+++ components/layoutLogin.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import styles from './layoutLogin.module.css'
 import { useRouter } from "next/router";
 import Link from 'next/link';
-export default function LayoutLogin({ children, href }) {
+import AlertBox from './compose/alertBox';
+export default function LayoutLogin({ children }) {
   const router = useRouter();
   return (
     <>
@@ -17,10 +18,17 @@ export default function LayoutLogin({ children, href }) {
             {children}
           </div>
           {
+            children.props.showAlert ?
+              <AlertBox clickOut={(e) => children.props.setShowAlert(!e)}>
+                {children.props.AlertInner}
+              </AlertBox>
+              : null
+          }
+          {
             router.pathname == `/` ? <>
               <p className={styles.textNotCreateNow}>Don’t have any account yet?</p>
               <Link href={'register'}>
-              <p className={styles.textCreateNow}>Create account</p>
+                <p className={styles.textCreateNow}>Create account</p>
               </Link>
             </>
               : null
