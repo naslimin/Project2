e8348c56b230f08c0c2eb34743fd440e45b3adff
Naslimin <naslimin@gmail.com>
2022-02-27 02:10:04 +0700

    add onchange and name

--- components/compose/inputType/index.js
+++ components/compose/inputType/index.js
@@ -1,9 +1,9 @@
 import styles from './index.module.css'
 
-export default function InputType({ type, value, placeholder,className }) {
+export default function InputType({ type, value, placeholder,className ,name ,onChange}) {
   return (
     <>
-      <input className={`${styles.inputType} ${className}`} type={type} value={value} placeholder={placeholder} autoComplete="new-password"></input>
+      <input className={`${styles.inputType} ${className}`} name={name} type={type} onChange={onChange} value={value} placeholder={placeholder} autoComplete="new-password"></input>
     </>
   )
 }
