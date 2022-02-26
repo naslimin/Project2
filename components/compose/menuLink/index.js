079ae85d9ffd9159156ee0af3bbbb8eeefb38a8c
Naslimin <naslimin@gmail.com>
2022-02-27 02:09:35 +0700

    logout function

--- components/compose/menuLink/index.js
+++ components/compose/menuLink/index.js
@@ -2,15 +2,37 @@
 import styles from './index.module.css'
 import Link from 'next/link'
 import { useRouter } from "next/router";
-export default function MenuLink({ to, icon, children }) {
+import fire from '../../../config/firebase';
+export default function MenuLink({ to, icon, children , props}) {
+
   const router = useRouter();
-  return (
-    <Link href={to}>
+  const logoutFN = async () => {
+    props.props.setAlertInner(<><img width={60} height={60} src='/icon/loading-buffering.gif' /></>)
+    props.props.setShowAlert(true);
+    await fire.auth().signOut()
+    setTimeout(()=>{
+      props.props.setShowAlert(false);
+      router.push('/')
+    },1500)
+  }
+  if (to == '/logout') {
+    return (
       <div
+        onClick={() => logoutFN()}
         className={router.pathname == `/${to}` ? `${styles.menuLink} ${styles.active}` : styles.menuLink}>
         <img src={icon} alt="icon menu" width="30" height="30" />
         <div className={styles.menuLabel}>{children}</div>
       </div>
-    </Link>
-  )
+    )
+  } else {
+    return (
+      <Link href={to}>
+        <div
+          className={router.pathname == `/${to}` ? `${styles.menuLink} ${styles.active}` : styles.menuLink}>
+          <img src={icon} alt="icon menu" width="30" height="30" />
+          <div className={styles.menuLabel}>{children}</div>
+        </div>
+      </Link>
+    )
+  }
 }
