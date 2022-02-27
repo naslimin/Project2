e77647c387d8e17273f4a55661ba2f544d8f6478
Naslimin <naslimin@gmail.com>
2022-02-27 12:06:30 +0700

    check logout function

--- components/compose/EventBox/index.js
+++ components/compose/EventBox/index.js
@@ -2,12 +2,19 @@
 import styles from './index.module.css'
 import moment from 'moment';
 moment.locale('en-GB');
-export default function EventBox({ element }) {
+export default function EventBox({ element, props }) {
+  const ShowStatus = () => {
+    if (element.type == 'checkin') {
+      return <span className={`${styles.textMain} ${styles.checkintype} ${styles.textBold} ${styles.checkin}`}>{element.desc}</span>
+    } else {
+      return <span className={`${styles.textMain} ${styles.textBold}`}>{element.desc}</span>
+    }
+  }
   return (
     <div className={styles.warper}>
-      <div className={styles.warperOptions}>
+      {/* <div className={styles.warperOptions}>
         <img src='/icon/options.png' width={20} height={20} ></img>
-      </div>
+      </div> */}
       <div className={`${styles.warperBox}`}>
         <div className={`${styles.warperBoxBody}`}>
           <p className={`${styles.text} ${styles.textMain}`}>วันที่/เดือน/ปี</p>
@@ -17,15 +24,20 @@ export default function EventBox({ element }) {
             <span className={`${styles.textSub} ${styles.textP}`}>เข้า</span>
             <span className={`${styles.textMain} ${styles.textBold} ${styles.textP}`}>{moment(element.start).format('HH:mm')}</span>
             <span className={`${styles.textSub} ${styles.textP}`}>ออก</span>
-            <span className={`${styles.textMain} ${styles.textBold}`}>{moment(element.end).format('HH:mm')}</span>
+            {
+              element.end ?
+                <span className={`${styles.textMain} ${styles.textBold}`}>{moment(element.end).format('HH:mm')}</span>
+                :
+                <span className={`${styles.textMain} ${styles.textBold} ${styles.textRed}`}>รอบันทึกเวลาออก</span>
+            }
           </p>
           <p className={`${styles.text} ${styles.mtauto}`}>
             <span className={`${styles.textSub} ${styles.textP}`}>สถานะ:</span>
-            <span className={`${styles.textMain} ${styles.textBold}`}>บันทึกแล้ว</span>
+            <ShowStatus />
           </p>
         </div>
         <div className={`${styles.warperBoxBodyImage}`}>
-          <img src='/icon/Signature.png' width={160} height={91} ></img>
+          <img src={props.UserDataDetail.Signature} width={160} height={91}></img>
           <p className={`${styles.text} {styles.textMain}`}>-Signature-</p>
         </div>
       </div>
