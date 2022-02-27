70d9dabb172c49354beed0a76fd19b489d9a9d75
Naslimin <naslimin@gmail.com>
2022-02-27 12:06:42 +0700

    add onchange

--- components/compose/inputTypeForm/index.js
+++ components/compose/inputTypeForm/index.js
@@ -1,9 +1,9 @@
 import styles from './index.module.css'
 
-export default function InputTypeForm({ type, value, placeholder,className,name }) {
+export default function InputTypeForm({ type, value, placeholder,className,name,onChange }) {
   return (
     <>
-      <input className={`${styles.inputType} ${className}`} name={name} type={type} value={value} placeholder={placeholder} autoComplete="new-password"></input>
+      <input className={`${styles.inputType} ${className}`} name={name} type={type} onChange={onChange} value={value} placeholder={placeholder} autoComplete="new-password"></input>
     </>
   )
 }
